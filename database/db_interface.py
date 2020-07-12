92c8d242673e8dffa2ea9d7f252b91943092dc7a
Jonathan <jjonathanlow@gmail.com>
2020-07-11 19:13:02 -0700

    started workin on database

---
+++ database/db_interface.py
@@ -0,0 +1,14 @@
+import sqlite3
+conn = sqlite3.connect('users.db')
+
+def add_user(phone_num, state, county):
+    #add error handling
+    pass
+
+
+def del_user(phone_num):
+    #add error handling
+    pass
+
+
+
