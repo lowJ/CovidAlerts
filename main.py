3990017eadaa15ea11874cd1d2ff81f171b2c536
Jonathan <jjonathanlow@gmail.com>
2020-07-11 20:14:00 -0700

    added main run script

---
+++ main.py
@@ -0,0 +1,4 @@
+import twillio.sms
+import database.db_interface
+
+#Code for scheudling text messages
--- twillio/main.py
+++
@@ -1,4 +0,0 @@
-import sms
-import twillio_creds
-
-sms.send_message(twillio_creds.jwl_num, "test_message")
