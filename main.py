3fa6330188a1bda1b2d5fb2da79a7beaf057a302
Jonathan <jjonathanlow@gmail.com>
2020-07-11 21:12:24 -0700

    reformatted

--- main.py
+++ main.py
@@ -1,4 +1,19 @@
-import twillio.sms
-import database.db_interface
+import sms
+import db_interface
+
+#import trungs function
+
 
 #Code for scheudling text messages
+
+#send at x:xx pm every day
+
+def helper(phone, state, county):
+    #trungs function
+    cases = 10
+    deaths = 10
+    to_send = 10
+    sms.send_message(phone, "hiii")
+
+
+db_interface.call_on_all_phone_num(helper)
--- sms.py
+++ sms.py
@@ -4,8 +4,6 @@ import twillio_creds
 account_sid = twillio_creds.twillio_acc_sid
 auth_token = twillio_creds.twillio_auth
 
-
-
 def send_message(num, msg):
     client = Client(account_sid, auth_token)
 
