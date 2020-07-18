1f168159318e9e85226df7ef49ad30a1150c09cd
Jonathan <jjonathanlow@gmail.com>
2020-07-18 00:13:47 -0700

    started website layout with bootstrap studio

--- web.py
+++ web.py
@@ -5,8 +5,8 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     #update stats and county list 
-    states_list = ["1", "2", "3"] #create function that returns list of all states
-    counties_list = ["a", "b", "c"] 
+    states_list = ["10, me", "2,w e", "3"] #create function that returns list of all states
+    counties_list = ["la,ca", "lawdale,colorado", "new york,new york", "new york,new york", "new york,new york", "new york,new york", "new york,new york", "new york,new york", "new york,new york", "new york,new york", "new york,new york", "new york,new york", "new york,new york", "new york,new york", "new york,new york", "new york,new york", "new york,new york", "new york,new york", "new york,new york", "new york,new york", "new york,new york", "new york,new york", "new york,new york", "new york,new york", "new york,new york", "new york,new york"] 
     
     #check that list value is correct
     return render_template("index.html", states = states_list, counties = counties_list)
