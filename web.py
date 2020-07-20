234e342e54a6bc39db671fa6a881bf53785eac2a
Jonathan <jjonathanlow@gmail.com>
2020-07-19 22:55:59 -0700

    fixed error on counties that had spaces

--- web.py
+++ web.py
@@ -1,13 +1,13 @@
 from flask import Flask, redirect, url_for
 from flask import render_template
+import data_processing
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     #update stats and county list 
     states_list = ["10, me", "2,w e", "3"] #create function that returns list of all states
-    counties_list = ["la,ca", "lawdale,colorado", "new york,new york", "new york,new york", "new york,new york", "new york,new york", "new york,new york", "new york,new york", "new york,new york", "new york,new york", "new york,new york", "new york,new york", "new york,new york", "new york,new york", "new york,new york", "new york,new york", "new york,new york", "new york,new york", "new york,new york", "new york,new york", "new york,new york", "new york,new york", "new york,new york", "new york,new york", "new york,new york", "new york,new york"] 
-    
+    counties_list = data_processing.listcounties()
     #check that list value is correct
     return render_template("index.html", states = states_list, counties = counties_list)
 
