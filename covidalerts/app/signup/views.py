6dc7e2429f0a5cfd631e62b16c699911f213892d
Vakhshoori101 <vakhshoori101@gmail.com>
2020-08-05 15:29:37 -0700

    Added error handling for phone number and county

--- covidalerts/app/signup/views.py
+++ covidalerts/app/signup/views.py
@@ -13,10 +13,37 @@ from signup.countyCheck import checkCounty
 def home(request):
     if(request.method == "POST"):
         post_data = request.POST
-        msg = post_data['county'] + post_data['phone_num']
+
+        phone_num = post_data['phone_num']
+        county_get = post_data['county']
+
+        # check if phone number is valid
+        phone_is_valid = checkNumber(phone_num)
+        if phone_is_valid:
+            phone_num = formatNumber(phone_num)
+
+        # check if county is valid
+        county_get = checkCounty(county_get)
+        if type(county_get) is str:
+            county_is_valid = False
+        else:
+            county_is_valid = True
+            county, state = county_get
+
+        if not county_is_valid:
+            msg = county_get
+        elif not phone_is_valid:
+            msg = 'Invalid phone number.'
+        elif 0: # need to check if phone num and county already exists in database
+            msg = f'{phone_num} is already registered for {county_get} county.'
+        else:
+            msg = f'Successfully signed up {phone_num} to recieve alerts for {county}, {state}.'
+
+        # msg = post_data['county'] + post_data['phone_num']
         context = {
             'msg' : msg
         }
+
         return render(request, 'signup/success.html', context)
     
     counties_list = listcounties()
