db6d7d35d981b549005a11b1a3f2d8cbc4ce52d3
Vakhshoori101 <vakhshoori101@gmail.com>
2020-09-05 12:26:02 -0700

    Deleted extra app folder

---
+++ creds.py
@@ -0,0 +1,4 @@
+twillio_acc_sid = "AC9fab263e2fc314671146a3a270533b01"
+twillio_auth = "db9f5594897cca52812d5dfd5fd30fec"
+
+from_number = "17865634906"
--- signup/sms.py
+++ signup/sms.py
@@ -1,13 +1,13 @@
 from twilio.rest import Client
-import twillio_creds
+import creds
 
-account_sid = twillio_creds.twillio_acc_sid
-auth_token = twillio_creds.twillio_auth
+account_sid = creds.twillio_acc_sid
+auth_token = creds.twillio_auth
 
 def send_message(num, msg):
     client = Client(account_sid, auth_token)
 
     client.api.account.messages.create(
         to="+1" + num,
-        from_="+" + twillio_creds.from_number,
+        from_="+" + creds.from_number,
         body=msg)
